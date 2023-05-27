c9543018cbe0ad556d26772a3979de10e6b36664
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2023-05-27 18:52:19 -0500

    Initial Commit

---
+++ Pass Generator/Pass_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pass_GeneratorApp.swift
+//  Pass Generator
+//
+//  Created by Jonathan Daniel on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Pass_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
