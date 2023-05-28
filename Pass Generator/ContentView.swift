f60fde7f123264c2505289abb53ba479d2a785b6
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2023-05-27 20:17:11 -0500

    populate

--- Pass Generator/ContentView.swift
+++ Pass Generator/ContentView.swift
@@ -6,16 +6,89 @@
 //
 
 import SwiftUI
+import PassKit
 
 struct ContentView: View {
+    
+    @State private var qrText = "YO"
+    @State private var thumbnailImageLink = "";
+    
+    @State private var primaryLabel = ""
+    @State private var primaryValue = ""
+    
+    @State private var secondaryLabel1 = ""
+    @State private var secondaryValue1 = ""
+    
+    @State private var secondaryLabel2 = ""
+    @State private var secondaryValue2 = ""
+    
+    @State private var auxiliaryLabel1 = ""
+    @State private var auxiliaryValue1 = ""
+    
+    @State private var auxiliaryLabel2 = ""
+    @State private var auxiliaryValue2 = ""
+    
+    @State private var backgroundColor = Color.white
+    @State private var textColor = Color.black
+    
+    @State private var isLoading = false;
+    
+    @State private var newPass: PKPass?
+    
+    @State private var passSheetVisible = false;
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Form {
+                Section("content") {
+                    TextField("QR Text", text: self.$qrText)
+                    TextField("Thumbnail Image URL", text: self.$thumbnailImageLink)
+                }
+                
+                Section("Primary Fields") {
+                    TextField("Label", text: self.$primaryLabel)
+                    TextField("Value", text: self.$primaryValue)
+                }
+                
+                Section("Secondary Fields") {
+                    TextField("Label 1", text: self.$secondaryLabel1)
+                    TextField("Value 1", text: self.$secondaryValue1)
+                    
+                    TextField("Label 2", text: self.$secondaryLabel2)
+                    TextField("Value 2", text: self.$secondaryValue2)
+                    
+                }
+                
+                Section("Auxiliary Fields") {
+                    TextField("Label 1", text: self.$auxiliaryLabel1)
+                    TextField("Value 1", text: self.$auxiliaryValue1)
+                    
+                    TextField("Label 2", text: self.$auxiliaryLabel2)
+                    TextField("Value 2", text: self.$auxiliaryValue2)
+                    
+                }
+                
+                Section("Colors") {
+                    ColorPicker("Background Color", selection: self.$backgroundColor)
+                    ColorPicker("Text Color", selection: self.$textColor)
+                }
+                
+                Section("Add to wallet") {
+                    if(!self.isLoading) {
+                        AddPassToWalletButton {
+                            print("add pass")
+                        }
+                    } else {
+                        ProgressView()
+                    }
+                }
+                
+            }
+            .navigationTitle("Pass Generator")
+            .sheet(isPresented: self.$passSheetVisible) {
+                
+            }
         }
-        .padding()
     }
 }
 
